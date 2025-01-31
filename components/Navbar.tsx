c34fdccf18c22cba5595a880983597f82041e889
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2025-02-01 02:39:25 +0500

    new logo added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = async () => {
     return (
         <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
             <nav className='flex justify-between items-center'>
-                <Link href="/">
+                <Link href="/Logo.svg">
                     <Image src="/" alt="logo" width={144} height={30} />
                 </Link>
 
