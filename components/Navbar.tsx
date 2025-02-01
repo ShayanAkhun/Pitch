2ccf7667f510c79e7bcf129ffa9180dfd4588385
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2025-02-01 12:07:24 +0500

    logo fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,8 +12,8 @@ const Navbar = async () => {
     return (
         <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
             <nav className='flex justify-between items-center'>
-                <Link href="/Logo.svg">
-                    <Image src="/" alt="logo" width={144} height={30} />
+                <Link href="/">
+                    <Image src="/Logo.svg" alt="logo" width={144} height={30} />
                 </Link>
 
                 <div className='flex items-center gap-5 text-black'>
